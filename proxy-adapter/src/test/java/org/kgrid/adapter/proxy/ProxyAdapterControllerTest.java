5f4a46cb65288df07473bf117ebbf5211cddbff0
ngittlen <nggittle@med.umich.edu>
2021-02-04 14:39:13 -0500

    Convert tests to junit 5

--- proxy-adapter/src/test/java/org/kgrid/adapter/proxy/ProxyAdapterControllerTest.java
+++ proxy-adapter/src/test/java/org/kgrid/adapter/proxy/ProxyAdapterControllerTest.java
@@ -1,32 +1,29 @@
 package org.kgrid.adapter.proxy;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 
 import java.io.ByteArrayInputStream;
 import java.net.URI;
 import javax.servlet.http.HttpServletRequest;
-import org.junit.Test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.kgrid.adapter.api.ActivationContext;
 
+@DisplayName("Proxy Adapter Controller Tests")
 public class ProxyAdapterControllerTest {
 
-  public void setUp() throws Exception {
-  }
-
   @Test
+  @DisplayName("Get code artifact returns binary")
   public void testGetCodeArtifactHandlesActualNameIsProxy() {
-    // given
     HttpServletRequest req = mock(HttpServletRequest.class);
     ActivationContext ctx = mock(ActivationContext.class);
     ProxyAdapter adapter = new ProxyAdapter();
     adapter.initialize(ctx);
 
-    // and
     given(ctx.getBinary(any(URI.class))).willReturn(new ByteArrayInputStream("Hi, Bob".getBytes()));
     given(req.getRequestURI())
         .willReturn("/proxy/artifacts/proxy/name/version/src/index.js")
@@ -36,7 +33,6 @@ public class ProxyAdapterControllerTest {
 
     then(ctx).should().getBinary(URI.create("proxy/name/version/src/index.js"));
 
-    // and also
     adapter.getCodeArtifact(req);
 
     then(ctx).should().getBinary(URI.create("naan/name/version/src/index.js"));
